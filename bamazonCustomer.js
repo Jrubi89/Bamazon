dcc276c1d993cc35fefa60fdd613fcc568f1f09a
unknown <jrubi89@icloud.com>
2020-06-24 20:54:25 -0400

    creating a variable to connect to mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,12 @@
-//Require packages//
+//Conencting require packages//
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table2");
-
-
+//Connection variable to connect to sql database//
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Pe@ches21489",
+  database: "bamazon_db",
+  port: 3306
+});
