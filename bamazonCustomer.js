e50736069ee802d17e3d0ccd1a13297ab1f314bb
unknown <jrubi89@icloud.com>
2020-06-24 21:45:48 -0400

    Shopping function created

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,8 +38,76 @@ var display = function() {
     for (var i = 0; i < res.length; i++) {
       table.push([res[i].id, res[i].product_name, res[i].price]);
     }
-//Console log table//    
+//Console log table//
     console.log(table.toString());
     console.log("");
     shopping();
-  }); //End Connection to product//
+  }); //End Connection to products//
+};
+//Shopping variable//
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "productToBuy",
+      type: "input",
+      message: "Please enter the Product Id of the item you wish to purchase.!"
+    })
+    .then(function(answer1) {
+      var selection = answer1.productToBuy;
+      connection.query("SELECT * FROM products WHERE Id=?", selection, function(
+        err,
+        res
+      ) {
+        if (err) throw err;
+        if (res.length === 0) {
+          console.log(
+            "That Product doesn't exist, Please enter a Product Id from the list above"
+          );
+
+          shopping();
+        } else {
+          inquirer
+            .prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many items woul you like to purchase?"
+            })
+            .then(function(answer2) {
+              var quantity = answer2.quantity;
+              if (quantity > res[0].stock_quantity) {
+                console.log(
+                  "Our Apologies we only have " +
+                    res[0].stock_quantity +
+                    " items of the product selected"
+                );
+                shopping();
+              } else {
+                console.log("");
+                console.log(res[0].products_name + " purchased");
+                console.log(quantity + " qty @ $" + res[0].price);
+
+                var newQuantity = res[0].stock_quantity - quantity;
+                connection.query(
+                  "UPDATE products SET stock_quantity = " +
+                    newQuantity +
+                    " WHERE id = " +
+                    res[0].id,
+                  function(err, resUpdate) {
+                    if (err) throw err;
+                    console.log("");
+                    console.log("Your Order has been Processed");
+                    console.log("Thank you for Shopping with us...!");
+                    console.log("");
+                    connection.end();
+                  }
+                );
+              }
+            });
+        }
+      });
+    });
+};
+
+display();
+
+
